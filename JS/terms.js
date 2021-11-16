5140f3d647a10ee1c32d62218e7adbc923ad9790
David Saavedra Vazquez <davidsv20@icloud.com>
2021-11-15 18:34:28 -0600

    Changes made in terms.js and main.scss

--- JS/terms.js
+++ JS/terms.js
@@ -18,7 +18,28 @@ function show_terms() {
             searches.appendChild(a);
             console.log(product);
             a.addEventListener("click", () => {
-                
+                async function searchGif() {
+                    let search = a.text;
+                    console.log(search); 
+                    //request parameters: api_key and q (string)
+                    let search_fetch = `${searchUrl}&api_key=${apiKey}&q=${search}&limit=20&offset=0`;
+                    console.log(search_fetch);
+                    const response = await fetch(search_fetch);
+                    const data = await response.json();
+                    console.log(data);
+                    return data;
+                }
+                searchGif().then(response => {
+                    for (var i = 0; i < response.data.length; i++) {
+                        //Gifs container
+                        let foundGif = document.createElement('img');
+                        foundGif.setAttribute('src', response.data[i].images.original.url);
+                        gifs.appendChild(foundGif);
+                        foundGif.style.width = '300px';
+                    }
+                }).catch(err => {
+                    console.error('something went wrong :/', err);
+                })
             })
          });
 
