7d6e32dfde0ff83cb1ebeb4d95a3159a4e8f78dc
David Saavedra Vazquez <davidsv20@icloud.com>
2022-03-08 23:51:46 -0600

    Layout1 and layout2 functions finished in responsive.js

--- JS/responsive.js
+++ JS/responsive.js
@@ -28,12 +28,12 @@ const layout1 = () => {
         resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 180vh 10vh';
     } else if(width1 >=1150 && width1 < 1400) {
         console.log("This is a laptop");
-        document.body.style.gridTemplateRows = '10vh 310vh 20vh';
-        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 140vh 10vh';
+        document.body.style.gridTemplateRows = '10vh 320vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 160vh 10vh';
     } else if(width1 >= 1400) {
         console.log("This is a larger laptop or a tv");
-        document.body.style.gridTemplateRows = '10vh 280vh 20vh';
-        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 110vh 10vh';
+        document.body.style.gridTemplateRows = '10vh 300vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 130vh 10vh';
     }
 }
 const layout2 = () => {
@@ -41,18 +41,36 @@ const layout2 = () => {
     console.log(searchHandler1);
     console.log(searchHandler2);
     const width2 = window.innerWidth;
-    if(width2 >= 300 && width2 < 425) {
-        console.log("This is a phone2");
-        document.body.style.gridTemplateRows = '10vh 420vh 20vh';
-        searchContainer.style.gridTemplateRows = '20vh 30vh 30vh 10vh 260vh 0vh';
-    } else if(width2 >= 425 && width2 < 768) {
-        console.log("This is a large mobile2")
-    } else if(width2 >=768 && width2 < 1020) {
-        console.log("This is a tablet2");
-    } else if(width2 >=1020 && width2 < 2560) {
-        console.log("This is a laptop2");
-    } else if(width2 >= 2560) {
-        console.log("This is a larger laptop or a tv2");
+    if(width2 >= 300 && width2 < 450) {
+        console.log("This is a phone");
+        // document.classList.toggle("mobile-body");
+        // resultContainer.classList.toggle("mobile-container");
+        document.body.style.gridTemplateRows = '10vh 410vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 30vh 30vh 10vh 250vh 10vh';
+    } else if(width2 >= 450 && width2 < 600) {
+        console.log("This is a large mobile");
+        document.body.style.gridTemplateRows = '10vh 310vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 30vh 30vh 10vh 150vh 10vh';
+    } else if(width2 >= 600 && width2 < 750) {
+        console.log("This is a tablet");
+        document.body.style.gridTemplateRows = '10vh 570vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 390vh 10vh';
+    } else if(width2 >= 750 && width2 < 1000) {
+        console.log("This is a tablet");
+        document.body.style.gridTemplateRows = '10vh 430vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 260vh 10vh';
+    } else if(width2 >=1000 && width2 < 1155) {
+        console.log("This is a laptop");
+        document.body.style.gridTemplateRows = '10vh 480vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 310vh 10vh';
+    } else if(width2 >=1155 && width2 < 1400) {
+        console.log("This is a laptop");
+        document.body.style.gridTemplateRows = '10vh 490vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 320vh 10vh';
+    } else if(width2 >= 1400) {
+        console.log("This is a larger laptop or a tv");
+        document.body.style.gridTemplateRows = '10vh 400vh 20vh';
+        resultContainer.style.gridTemplateRows = '20vh 40vh 30vh 10vh 230vh 10vh';
     }
 }
 const selectLayout = () => {
